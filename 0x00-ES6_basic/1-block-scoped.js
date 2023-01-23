1c9eedee4d58690e55aa17d31858cfc88f25850a
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-01-23 17:52:50 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock (trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
   }
 
   return [task, task2];
