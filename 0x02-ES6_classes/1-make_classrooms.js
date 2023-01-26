d2a3c4b540708d2e58e16c7c4ac278fb2ab90dca
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-01-26 12:39:22 +0000

    getters/setters

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,9 @@
 import ClassRoom from './0-classroom';
 
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
 export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
