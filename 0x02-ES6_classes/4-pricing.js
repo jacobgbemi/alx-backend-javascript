a510d75656694247f38261c5bbb4dd2e1aa0821d
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-01-26 16:26:35 +0000

    inheritance

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,35 +1,50 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor (amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  // Getters and setters
-  get amount () {
+  get amount() {
     return this._amount;
   }
 
-  set amount (value) {
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
     this._amount = value;
   }
 
-  get currency () {
+  /**
+   * @returns {Currency}
+   */
+  get currency() {
     return this._currency;
   }
 
-  set currency (value) {
+  /**
+   * @param {Currency} value
+   */
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
     this._currency = value;
   }
 
-  // Method to display the full price
-  displayFullPrice () {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
-  // Static method to convert price
-  static convertPrice (amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
     return amount * conversionRate;
   }
 }
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,20 @@
 export default class Building {
-  constructor (sqft) {
+  constructor(sqft) {
     this.sqft = sqft;
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          'Class extending Building must override evacuationWarningMessage'
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
   }
 
-  get sqft () {
+  get sqft() {
     return this._sqft;
   }
 
-  set sqft (value) {
+  set sqft(value) {
     this._sqft = value;
   }
 }
