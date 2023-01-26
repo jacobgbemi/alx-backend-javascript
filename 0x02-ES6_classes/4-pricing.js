bb2930a41bb6015f967a8ba07b2cd55ae56172d0
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-01-26 16:04:45 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor (amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters and setters
+  get amount () {
+    return this._amount;
+  }
+
+  set amount (value) {
+    this._amount = value;
+  }
+
+  get currency () {
+    return this._currency;
+  }
+
+  set currency (value) {
+    this._currency = value;
+  }
+
+  // Method to display the full price
+  displayFullPrice () {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice (amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
