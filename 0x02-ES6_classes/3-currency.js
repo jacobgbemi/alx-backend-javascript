8237d7347412c7229bc9a8b905c614b8fb44b2fa
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-01-26 15:54:43 +0000

    fixing

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,26 @@
 export default class Currency {
-constructor(code, name) {
-this._code = code;
-this._name = name;
-}
-get code() {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
     return this._code;
-}
+  }
 
-set code(newCode) {
+  set code(newCode) {
     this._code = newCode;
-}
+  }
 
-get name() {
+  get name() {
     return this._name;
-}
+  }
 
-set name(newName) {
+  set name(newName) {
     this._name = newName;
-}
+  }
 
-displayFullCurrency() {
+  displayFullCurrency() {
     return `${this._name} (${this._code})`;
-}
-
+  }
 }
