8ad6a54bf44b3d532bc0b6faa04db9bca15bdb36
Gbemi Adebayo <adebayogbemi90@gmail.com>
2023-02-08 16:52:19 +0100

    task 9 and 10

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingC > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,19 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
