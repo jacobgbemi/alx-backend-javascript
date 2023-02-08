e883d7f0cea55fe2eb0e9a341921a551c2de2da8
Gbemi Adebayo <adebayogbemi90@gmail.com>
2023-02-08 16:24:02 +0100

    task 5, 6 and 7

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,77 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return('Cannot work from home');
+  }
+  getCoffeeBreak(): string {
+    return('Cannot have a break');
+  }
+  workTeacherTasks(): string {
+    return('Getting to work');
+  }
+}
+
+const createEmployee = (salary: number | string) => {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher;
+  } else {
+    return new Director;
+  }
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  // return (employee as DirectorInterface).workDirectorTasks !== undefined;
+  return (employee instanceof Director);
+
+
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface) {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects) {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
