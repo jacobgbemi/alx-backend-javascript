b75887226550477f7f028808bdc82feb565e4900
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-03-21 08:41:29 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * 
+ *
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   fs.readFile(dataPath, 'utf-8', (err, data) => {
